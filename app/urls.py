645a016aa0a2ba887097490ed25507847ebee7de
112sonali <sonaliojha1211@gmail.com>
2024-08-03 21:48:54 +0530

    create music app

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("",index),
+    path("addsong/", addsong),
+    path("song/", song),
+    path("update_song/<int:uid>/",update_song, name="update_song"),
+    path("update_song_data/", update_song_data),
+    path("delete_song/<int:pk>/", delete_song, name="delete_song"),
+    path("download_song/<int:pk>/", download_song, name="download_song"),
+]+ static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
